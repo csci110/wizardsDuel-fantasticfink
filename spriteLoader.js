d8fb59b2eea185dc3f1306e92aebd70367ef48dd
Rob Fink <finkr@dewv.edu>
2018-08-31 16:33:57 +0000

    Updates via sync script

---
+++ spriteLoader.js
@@ -0,0 +1,11 @@
+import {game} from "./sgc/sgc.js";
+
+game.preloadImage("floor.png");
+game.preloadImage("horizontalWall.png");
+game.preloadImage("verticalWall.png");
+game.preloadImage("marcus.png", 48, 48);
+game.preloadImage("stranger.png", 48, 48);
+game.preloadImage("ann.png", 48, 48);
+game.preloadImage("rfsprite.png");
+game.preloadImage("rfbackground.jpg");
+
